b767bac2f56b8fd16178733c474a8f9e9387cafd
yoyc <40702947+yoyc@users.noreply.github.com>
2020-04-27 09:18:29 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$site_name = ""; //填入你的站点名，例如 https://214214.xyz/tools/dlink/
-$cf_workers = ""; //填入搭建的cf-worker链接,否则将默认使用作者所搭建的
+$site_name = "https://dlink.170828.xyz/"; //填入你的站点名，例如 https://214214.xyz/tools/dlink/
+$cf_workers = "https://sw.unsc.workers.dev"; //填入搭建的cf-worker链接,否则将默认使用作者所搭建的
 ?>
